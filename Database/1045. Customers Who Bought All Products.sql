b5b104b2f21837a51be275e4d8d80712722dc6e7
Akash Nair <47159738+Akash-Nair@users.noreply.github.com>
2025-05-24 23:34:44 -0700

    Update 1045. Customers Who Bought All Products.sql

--- Database/1045. Customers Who Bought All Products.sql
+++ Database/1045. Customers Who Bought All Products.sql
@@ -1,6 +1,2 @@
-select customer_id from (
-select distinct customer_id as customer_id, 
-group_concat(distinct product_key order by product_key separator ',') as abc
-from Customer
-group by customer_id) as xyz
-where xyz.abc = (select group_concat(distinct product_key order by product_key separator ',') from Product)
+SELECT  customer_id FROM Customer GROUP BY customer_id
+HAVING COUNT(distinct product_key) = (SELECT COUNT(product_key) FROM Product)
