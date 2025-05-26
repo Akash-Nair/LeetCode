fe1ea5196033b45b659263a0753c656eaff25bc7
Akash Nair <47159738+Akash-Nair@users.noreply.github.com>
2025-05-26 04:04:19 -0700

    Create 1321. Restaurant Growth.sql

---
+++ Database/1321. Restaurant Growth.sql
@@ -0,0 +1,6 @@
+select distinct 
+visited_on, 
+sum(amount) over (order by visited_on range between interval 6 day preceding and current row) as amount,
+round(sum(amount) over (order by visited_on range between interval 6 day preceding and current row)/7,2) as average_amount
+from Customer
+limit 6, 999
